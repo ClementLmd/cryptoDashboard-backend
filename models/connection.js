6ff89d92e0d763f771d14eb63366a615f83df37a
clement <clement.laumond@gmail.com>
2024-05-20 11:12:10 +0200

    changement de connection string

--- models/connection.js
+++ models/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+// const connectionString = "mongodb+srv://clementlaumond:qJLgJUTyHWJ70bgy@cluster0.xx4c2xo.mongodb.net/cryptoDashboard"
 const connectionString = process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
