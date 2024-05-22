d2c376ad690bbf4eaefc36dc92af3bf7bb1eeb9a
clement <clement.laumond@gmail.com>
2024-05-22 11:53:52 +0200

    modif routes pour meilleure correspondance avec le front

--- routes/users.js
+++ routes/users.js
@@ -28,7 +28,7 @@ router.post('/signup', (req, res) => {
           });
 
           newUser.save().then(newDoc => {
-            res.json({ result: true, token: newDoc.token });
+            res.json({ result: true, data: newDoc });
           });
 
         } else {
@@ -51,7 +51,7 @@ router.post('/signin', (req, res) => {
 
   User.findOne({ username: req.body.username }).then(data => {
     if (data && bcrypt.compareSync(req.body.password, data.password)) {
-      res.json({ result: true, token: data.token, username: data.username });
+      res.json({ result: true, data });
     } else {
       res.json({ result: false, error: 'User not found or wrong password' });
     }
--- routes/wallet.js
+++ routes/wallet.js
@@ -5,29 +5,30 @@ const Wallet = require('../models/wallets');
 
 router.post('/', (req, res) => {
     const { nameWallet, address, blockchain, user } = req.body
+    console.log("req.body", req.body)
+    Wallet.findOne({ address, user })
+        .populate('user').then(data => {
+            if (data === null) {
 
-    Wallet.findOne({ address, user }).then(data => {
-        if (data === null) {
+                const newWallet = new Wallet({
+                    nameWallet,
+                    address,
+                    blockchain,
+                    user,
+                });
 
-            const newWallet = new Wallet({
-                nameWallet,
-                address,
-                blockchain,
-                user,
-            });
+                newWallet.save().then(newDoc => {
+                    res.json({ result: true, newDoc });
+                });
 
-            newWallet.save().then(newDoc => {
-                res.json({ result: true, newDoc });
-            });
-
-        } else {
-            res.json({ result: false, error: 'Wallet already exists' });
-        }
-    })
+            } else {
+                res.json({ result: false, error: 'Wallet already exists' });
+            }
+        })
 });
 
-router.get('/', (req, res) => {
-    Wallet.find({ user: req.body.user })
+router.get('/:user', (req, res) => {
+    Wallet.find({ user: req.params.user })
         .populate('user')
         .then(listWallets => {
             if (listWallets) {
