6651d3d01f5468b1b0cc5dafeb22d097dd64af1f
clement <clement.laumond@gmail.com>
2024-05-24 11:40:53 +0200

    route put requêtes api btc,sol,eth terminée

--- routes/cryptos.js
+++ routes/cryptos.js
@@ -5,26 +5,26 @@ const Wallet = require('../models/wallets');
 const Crypto = require('../models/cryptos');
 const User = require('../models/users');
 
+const ETH_API_KEY="ZGJ85Q1912CX99S2A9JE6Q1VYPDVK9KC5S"
+
 router.put('/contentWallet', (req, res) => {
-    const { token } = req.body
+    const { token } = req.body;
     User.findOne({ token })
         .populate("wallets")
         .then(user => {
-            // console.log("user:", user)
-            const wallets = user.wallets
+            const wallets = user.wallets;
             const promises = wallets.map(wallet => {
-                const { blockchain, address } = wallet
+                const { blockchain, address } = wallet;
+                let cryptoName;
                 if (blockchain === "Solana") {
-                    const token = "SOL"
+                    cryptoName = "SOL";
                     const request = {
                         "jsonrpc": "2.0",
                         "id": 1,
                         "method": "getBalance",
-                        "params": [
-                            address
-                        ]
+                        "params": [address]
                     };
-                    fetch('https://api.mainnet-beta.solana.com', {
+                    return fetch('https://api.mainnet-beta.solana.com', {
                         method: 'POST',
                         headers: {
                             'Content-Type': 'application/json',
@@ -33,26 +33,24 @@ router.put('/contentWallet', (req, res) => {
                     })
                         .then(response => response.json())
                         .then(data => {
-                            if (data) {
-                                console.log('data réponse fetch api', data);
-                                const quantity = data.result.value / 1E9
-                                return Crypto.findOne({ token })
+                            if (data.error) {
+                                throw new Error('adresse solana invalide');
+                            }
+                            if (data.result) {
+                                const quantity = data.result.value / 1E9;
+                                return Crypto.findOne({ name: cryptoName })
                                     .then(crypto => {
-                                        console.log("crypto : ", crypto)
                                         if (!crypto) {
-                                            return console.log('Token not found');
+                                            throw new Error('token not found');
                                         }
-                                        const cryptoId = crypto._id
+                                        const cryptoId = crypto._id;
                                         return Wallet.updateOne(
                                             { address },
-                                            {
-                                                // $set: { "holdings.$.quantity": quantity }
-                                                $set: { holdings: [] }
-                                            }
+                                            { $set: { holdings: [] } }
                                         )
                                             .then(wallet => {
                                                 if (wallet.modifiedCount > 0) {
-                                                    console.log("wallet reinitialized")
+                                                    console.log("wallet reinitialized");
                                                 }
                                                 return Wallet.updateOne(
                                                     { address },
@@ -64,22 +62,113 @@ router.put('/contentWallet', (req, res) => {
                                                             }
                                                         }
                                                     }
-                                                ).then(data => {
-                                                    if (data.modifiedCount > 0) {
-                                                        console.log(`data wallet ${wallet}`, data)
+                                                ).then(updated => {
+                                                    if (updated.modifiedCount > 0) {
+                                                        console.log("wallet updated");
                                                     }
-                                                    else {
-                                                        console.log("erreur route put content wallet")
-                                                    }
-                                                });
-                                            })
-                                    })
+                                                })
+                                            });
+                                    });
                             }
                         })
-                } else {
-                    return Promise.reject(new Error("erreur fetch api solana"))
+                        .catch(error => {
+                            console.log(`erreur fetch api avec l'adresse ${address}: ${error.message}`)
+                            return Promise.reject(new Error(`erreur fetch api avec l'adresse ${address}: ${error.message}`));
+                        });
+                } else if (blockchain === "Bitcoin") {
+                    cryptoName = "BTC";
+                    return fetch(`https://blockchain.info/rawaddr/${address}`)
+                        .then(response => response.json())
+                        .then(data => {
+                            if (data.error) {
+                                throw new Error('adresse bitcoin invalide');
+                            }
+                            if (data.final_balance) {
+                                const quantity = data.final_balance / 1E8
+                                return Crypto.findOne({ name: cryptoName })
+                                    .then(crypto => {
+                                        if (!crypto) {
+                                            throw new Error('token not found');
+                                        }
+                                        const cryptoId = crypto._id;
+                                        return Wallet.updateOne(
+                                            { address },
+                                            { $set: { holdings: [] } }
+                                        )
+                                            .then(wallet => {
+                                                if (wallet.modifiedCount > 0) {
+                                                    console.log("wallet reinitialized");
+                                                }
+                                                return Wallet.updateOne(
+                                                    { address },
+                                                    {
+                                                        $push: {
+                                                            holdings: {
+                                                                crypto: cryptoId,
+                                                                quantity
+                                                            }
+                                                        }
+                                                    }
+                                                ).then(updated => {
+                                                    if (updated.modifiedCount > 0) {
+                                                        console.log("wallet updated");
+                                                    }
+                                                })
+                                            });
+                                    });
+                            }
+                        })
+                        .catch(error => {
+                            console.log(`erreur fetch api avec l'adresse ${address}: ${error.message}`)
+                            return Promise.reject(new Error(`erreur fetch api avec l'adresse ${address}: ${error.message}`));
+                        });
+                } else if (blockchain === "Ethereum") {
+                    cryptoName = "ETH";
+                    return fetch(`https://api.etherscan.io/api?module=account&action=balance&address=${address}&tag=latest&apikey=${ETH_API_KEY}`)
+                        .then(response => response.json())
+                        .then(data => {
+                            if (data.result) {
+                                const quantity = data.result / 1E18
+                                return Crypto.findOne({ name: cryptoName })
+                                    .then(crypto => {
+                                        if (!crypto) {
+                                            throw new Error('token not found');
+                                        }
+                                        const cryptoId = crypto._id;
+                                        return Wallet.updateOne(
+                                            { address },
+                                            { $set: { holdings: [] } }
+                                        )
+                                            .then(wallet => {
+                                                if (wallet.modifiedCount > 0) {
+                                                    console.log("wallet reinitialized");
+                                                }
+                                                return Wallet.updateOne(
+                                                    { address },
+                                                    {
+                                                        $push: {
+                                                            holdings: {
+                                                                crypto: cryptoId,
+                                                                quantity
+                                                            }
+                                                        }
+                                                    }
+                                                ).then(updated => {
+                                                    if (updated.modifiedCount > 0) {
+                                                        console.log("wallet updated");
+                                                    }
+                                                })
+                                            });
+                                    });
+                            }
+                        })
                 }
-            })
+                else {
+                    console.log(`autre blockchain: ${blockchain}`)
+                    return Promise.reject(new Error("autre blockchain"));
+                }
+            });
+
             Promise.allSettled(promises)
                 .then(results => {
                     const successfulUpdates = results.filter(result => result.status === 'fulfilled');
@@ -87,79 +176,6 @@ router.put('/contentWallet', (req, res) => {
                     res.json({ result: true, successfulUpdates, failedUpdates });
                 })
         })
-})
-
-// router.put('/contentWallet', (req, res) => {
-//     const { blockchain, address } = req.body
-//     if (blockchain === "Solana") {
-//         const token = "SOL"
-//         const request = {
-//             "jsonrpc": "2.0",
-//             "id": 1,
-//             "method": "getBalance",
-//             "params": [
-//                 address
-//             ]
-//         };
-//         fetch('https://api.mainnet-beta.solana.com', {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//             body: JSON.stringify(request),
-//         })
-//             .then(response => response.json())
-//             .then(data => {
-//                 if (data) {
-//                     console.log('data réponse fetch api', data);
-//                     const quantity = data.result.value / 1E9
-//                     Crypto.findOne({ token })
-//                         .then(crypto => {
-//                             console.log("crypto : ", crypto)
-//                             if (!crypto) {
-//                                 return res.json({ result: false, error: 'Token not found' });
-//                             }
-//                             const cryptoId = crypto._id
-//                             Wallet.updateOne(
-//                                 { address },
-//                                 {
-//                                     // $set: { "holdings.$.quantity": quantity }
-//                                     $set: { holdings: [] }
-//                                 }
-//                             )
-//                                 .then(wallet => {
-//                                     if (wallet.modifiedCount > 0) {
-//                                         console.log("wallet reinitialized")
-//                                     }
-//                                     Wallet.updateOne(
-//                                         { address },
-//                                         {
-//                                             $push: {
-//                                                 holdings: {
-//                                                     crypto: cryptoId,
-//                                                     quantity
-//                                                 }
-//                                             }
-//                                         }
-//                                     ).then(data => {
-//                                         if (data.modifiedCount > 0) {
-//                                             console.log("data", data)
-//                                             res.json({ result: true, data })
-//                                         }
-//                                         else {
-//                                             console.log("erreur route put content wallet")
-//                                             res.json({ result: false, error: "error route put content wallet" })
-//                                         }
-//                                     });
-//                                 })
-//                         })
-//                 }
-//             })
-
-
-//     } else res.json({ result: false, error: "erreur fetch api solana" })
-
-// })
-
+});
 
 module.exports = router;
