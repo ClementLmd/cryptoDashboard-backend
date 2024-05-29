a3bf01dab7c1a3e7a2f0ef7aa025003eab16dbb9
Tarek1808 <164371513+Tarek1808@users.noreply.github.com>
2024-05-29 11:22:10 +0200

    news ok regex on work

--- routes/news.js
+++ routes/news.js
@@ -6,12 +6,12 @@ const NEWS_API_KEY = process.env.NEWS_API_KEY;
 
 router.get("/", (req, res) => {
     console.log(NEWS_API_KEY);
-    fetch(`https://newsdata.io/api/1/news?apikey=${NEWS_API_KEY}&q=crypto&country=fr&language=en`)
+    fetch(`https://newsdata.io/api/1/news?apikey=${NEWS_API_KEY}&q=crypto&country=fr&language=fr`)
 
     .then(response => response.json())
-    .then(data =>{
-        if (data){  console.log(data)
-            res.json({result:true,news:data})
+    .then(dataN =>{
+        if (dataN){  console.log(dataN)
+            res.json({result:true,news:dataN})
         }else {
             res.json({result:false})
         }
